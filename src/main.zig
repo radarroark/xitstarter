812747a8ff07276c930a85f95c51cca0cf9fa5cb
radar roark <122068506+radarroark@users.noreply.github.com>
2025-03-02 19:00:25 -0500

    add xit as a library

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
+const xit = @import("xit");
+const rp = xit.repo;
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var work_dir = try std.fs.cwd().makeOpenPath("myrepo", .{});
+    defer work_dir.close();
+
+    var repo = try rp.Repo(.xit, .{}).init(allocator, .{ .cwd = work_dir }, ".");
+    defer repo.deinit();
+
+    try repo.addConfig(allocator, .{ .name = "user.name", .value = "mr magoo" });
+    try repo.addConfig(allocator, .{ .name = "user.email", .value = "mister@magoo" });
+
+    const readme = try work_dir.createFile("README.md", .{});
+    defer readme.close();
+    try readme.writeAll("hello, world!");
+    try repo.add(allocator, &.{"README.md"});
+
+    const oid = try repo.commit(allocator, .{ .message = "initial commit" });
+    std.debug.print("committed with object id: {s}\n", .{oid});
 }
