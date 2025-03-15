4129c74a6e1b0b075083900b0bb240d843323778
radar roark <122068506+radarroark@users.noreply.github.com>
2025-03-15 08:09:02 -0400

    update xit and use debug allocator

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const xit = @import("xit");
 const rp = xit.repo;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
