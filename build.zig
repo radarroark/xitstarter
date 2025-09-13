48544332dd95d4e6a794024832940b9cf4b4829b
radar roark <122068506+radarroark@users.noreply.github.com>
2025-09-13 18:07:56 -0400

    support zig 0.15

--- build.zig
+++ build.zig
@@ -7,9 +7,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "xitstarter",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     exe.root_module.addImport("xit", b.dependency("xit", .{}).module("xit"));
 
